eda722e57613343ad7c9a457bb495bb1a4c6228b
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2015-11-19 11:43:49 -0500

    remove HttpResponse from RestaurantAction Signed-off-by:Rugal Bernstein <rugal.bernstein.0@gmail.com>

--- src/main/java/ga/rugal/food/springmvc/controller/RestaurantAction.java
+++ src/main/java/ga/rugal/food/springmvc/controller/RestaurantAction.java
@@ -4,7 +4,6 @@ import config.SystemDefaultProperties;
 import ga.rugal.food.common.CommonLogContent;
 import ga.rugal.food.common.CommonMessageContent;
 import ga.rugal.food.core.dao.RestaurantDao;
-import ga.rugal.food.core.entity.Menu;
 import ga.rugal.food.core.entity.Restaurant;
 import ga.rugal.food.core.service.RestaurantService;
 import java.io.File;
@@ -30,48 +29,51 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping(value = "/restaurant")
-public class RestaurantAction 
+public class RestaurantAction
 {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(RestaurantAction.class.getName());
+
     private static final File imageFolder = new File(SystemDefaultProperties.IMAGE_FOLDER);
-    
+
     @Autowired
     private RestaurantService restaurantService;
-    
+
     @Autowired
     private RestaurantDao restaurantDao;
-    
+
     @Autowired
     private Random random;
-    
+
     @Autowired
     private ServletContext context;
-    
+
     /**
      * Find a restaurant randomly from table.
      * Get restaurant information through URL /restaurant
-     * 
-     * @return Give successful message and restaurant data in JSON format if exists, 
-     * otherwise return failed message
+     *
+     * @return Give successful message and restaurant data in JSON format if exists,
+     *         otherwise return failed message
      */
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET)
-    public Message randomRestaurant() {
-        
+    public Message randomRestaurant()
+    {
+
         int total = restaurantService.countTotal();
         LOG.debug(CommonLogContent.RESTAURANT_NUMBER, total);
-        
-        if (0 == total) {
-            
+
+        if (0 == total)
+        {
+
             LOG.warn(CommonLogContent.NO_RESTAURANT);
             return Message.failMessage(CommonMessageContent.RESTAURANT_NOT_FOUND);
         }
-        
+
         Restaurant restaurant = (Restaurant) restaurantService.getPage(random.nextInt(total), 1).getList().get(0);
         return Message.successMessage(CommonMessageContent.GET_RESTAURANT, restaurant);
     }
-    
+
     /**
      * GET image by the request restaurant id.
      * A default image will be returned if image cannot be found.
@@ -115,28 +117,31 @@ public class RestaurantAction
         response.setContentLength(data.length);
         return data;
     }
- 
+
     /**
      * Get a specific restaurant by restaurant id.
      * Return restaurant not found if the restaurant id is invalid.
+     *
      * @param rid
      * @param response
-     * @return Give successful message and restaurant data in JSON format if exists, 
-     * otherwise return failed message
+     *
+     * @return Give successful message and restaurant data in JSON format if exists,
+     *         otherwise return failed message
      */
     @ResponseBody
     @RequestMapping(value = "/{rid}")
-    public Message getSpecificRestaurant(@PathVariable("rid") Integer rid, HttpServletResponse response)
+    public Message getSpecificRestaurant(@PathVariable("rid") Integer rid)
     {
         Restaurant restaurant = restaurantDao.getByID(rid);
         //In case of invalid restaurant id
-        if (null == restaurant) {
+        if (null == restaurant)
+        {
             LOG.warn(CommonLogContent.NO_RESTAURANT);
             return Message.failMessage(CommonMessageContent.RESTAURANT_NOT_FOUND);
         }
-        
+
         return Message.successMessage(CommonMessageContent.GET_RESTAURANT, restaurant);
-        
+
     }
-        
+
 }
